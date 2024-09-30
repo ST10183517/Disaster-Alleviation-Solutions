8df203d3b4444bfe996a887c1e68a870d13c923d
jramu <jramukelani@gmail.com>
2024-09-30 13:41:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Disaster_Alleviation_Solutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
